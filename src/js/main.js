06e0a03307d784d516dd172994a04ac3d804a0de
Kaso <mackovision@gmail.com>
2024-01-12 11:35:59 -0800

    Added libs, built out starting point for calendar integration

--- src/js/main.js
+++ src/js/main.js
@@ -4,37 +4,7 @@
  *	GNU GPL 3.0 license
  */
 
- /** Imports */
-
-
-/** Variables and things */
-const s = '';
-let firstName, lastName;
-let element = document.getElementById("something");
-
-/** Clicks and clacks */
-spinner.addEventListener('click', () => {
-    // do something
-});
-
-spinner.addEventListener('touchstart', () => {
-   // do something
-});
-
-spinner.addEventListener('touchcancel', () => {
-    // do something
-});
-
-/**
- * Something
- * @param {string} foo - does something
- * @returns string
- */
-const something = (foo) => {
-    //do something
-
-    return foo;
-}
+ 
 
 /**
  * If document ready, GOOOOOOOOOOOOOOO!
